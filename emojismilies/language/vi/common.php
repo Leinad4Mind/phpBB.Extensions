d251cda73d4fed5a05f1192de325a6cfb90f67dd
Khoa Nguyen <nedkavn@gmail.com>
2016-11-29 05:53:08 +0700

    Emoji Smilies: Add Vietnamese language file

---
+++ emojismilies/language/vi/common.php
@@ -0,0 +1,106 @@
+<?php
+/**
+* This file is part of the Emoji Smilies package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+$lang = array_merge($lang, [
+	'EMOTICON_TEXT'	=> [
+		'ANGEL'					=> 'Thiên thần',
+		'ANGRY'					=> 'Nổi giận',
+		'ANGUISHED'				=> 'Đau khổ',
+		'ASTONISHED'			=> 'Kinh ngạc',
+		'BLUSH'					=> 'Mắc cỡ',
+		'CENSORED'				=> 'Bí mật',
+		'CLOWN'					=> 'Thằng hề',
+		'COLD'					=> 'Lạnh',
+		'COLD_MOUTH'			=> 'Đóng băng',
+		'CONFOUNDED'			=> 'Xấu hổ',
+		'CONFUSED'				=> 'Bối rối',
+		'COOL'					=> 'Quá đỉnh',
+		'COWBOY'				=> 'Cao bồi',
+		'CRY'					=> 'Hu hu',
+		'CRY_MAX'				=> 'Oa oa',
+		'DEVIL'					=> 'Quỷ sứ',
+		'DEVIL_IMP'				=> 'Chết tiệt',
+		'DISAPPOINTED'			=> 'Thất vọng',
+		'DISAPPOINTED_RELIEVED'	=> 'Hoang mang',
+		'DIZZY'					=> 'Chóng mặt',
+		'DROOL'					=> 'Biến thái',
+		'EXPRESSIONLESS'		=> 'Vô cảm',
+		'FEARFUL'				=> 'Đáng sợ',
+		'GRIMACE'				=> 'Nhăn nhó',
+		'GRIN'					=> 'He he',
+		'GRIN_TEARS'			=> 'Cười ra nước mắt',
+		'GRIN_WINK'				=> 'Hí hí',
+		'HEART'					=> 'Yêu quá',
+		'HUG'					=> 'Ôm ấp',
+		'HUNGRY'				=> 'Đói bụng',
+		'HUSHED'				=> 'Thẫn thờ',
+		'KEEP_CALM'				=> 'Kiềm chế',
+		'KISS'					=> 'Chụt chụt',
+		'KISS_BLUSH'			=> 'Hôn cái coi',
+		'KISS_LOVE'				=> 'Hôn gió',
+		'KISS_WINK'				=> 'Chu mỏ',
+		'LIE'					=> 'Chém gió',
+		'LOL'					=> 'Cười lăn lộn',
+		'MONEY'					=> 'Sang quá',
+		'NAUSEATED'				=> 'Buồn nôn',
+		'NERD'					=> 'Mọt sách',
+		'NEUTRAL'				=> 'Không liên quan',
+		'NO_MOUTH'				=> 'Miễn bàn',
+		'PENSIVE'				=> 'Suy tư',
+		'POUT'					=> 'Bĩu môi',
+		'RELIEVED'				=> 'Thở phào nhẹ nhõm',
+		'ROLLING_EYES'			=> 'Tròn xoe mắt',
+		'SAD'					=> 'Buồn',
+		'SAD_MOUTH'				=> 'Buồn não nề',
+		'SAD_WHITE'				=> 'Âu sầu',
+		'SCREAM'				=> 'Á á',
+		'SICK'					=> 'Im lặng',
+		'SICK_BANDAGE'			=> 'Bó đầu',
+		'SICK_FEVER'			=> 'Nóng trong người',
+		'SLEEP'					=> 'Khò khò',
+		'SLEEPY'				=> 'Buồn ngủ',
+		'SMILE'					=> 'Hi hi',
+		'SMILE_MOUTH'			=> 'Hì hì',
+		'SMILE_MOUTH_COLD'		=> 'Hừ hừ',
+		'SMILE_MOUTH_MAX'		=> 'Há há',
+		'SMILE_MOUTH_WINK'		=> 'Ha ha',
+		'SMILE_WHITE'			=> 'Vui',
+		'SMILE_WINK'			=> 'Cười mỉm',
+		'SMIRK'					=> 'Giả dối',
+		'SNEEZE'				=> 'Hắt xì',
+		'SURPRISED'				=> 'Ngạc nhiên',
+		'THINK'					=> 'Suy nghĩ',
+		'TIRED'					=> 'Mệt mỏi',
+		'TONGUE'				=> 'Le lưỡi',
+		'TONGUE_MAX'			=> 'Ứ chịu đâu',
+		'TONGUE_WINK'			=> 'Lêu lêu',
+		'UNAMUSED'				=> 'Chán phèo',
+		'UPSIDE'				=> 'Độn thổ',
+		'WEARY'					=> 'Chán đời',
+		'WINK'					=> 'Nháy mắt',
+		'WORRIED'				=> 'Lo lắng',
+		'WTF'					=> 'Đầu hàng'
+	]
+]);
