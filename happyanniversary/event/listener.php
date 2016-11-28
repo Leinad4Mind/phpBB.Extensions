5dfe18536dde3472d6aace52ce0f8d809a9df175
Khoa Nguyen <nedkavn@gmail.com>
2016-11-28 20:26:57 +0700

    Happy Anniversary: Fix the date on leap years

--- happyanniversary/event/listener.php
+++ happyanniversary/event/listener.php
@@ -10,61 +10,64 @@ namespace vinabb\happyanniversary\event;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+* PHP events
+*/
 class listener implements EventSubscriberInterface
 {
-	/** @var \phpbb\db\driver\driver_interface */
-	protected $db;
-
 	/** @var \phpbb\cache\driver\driver_interface */
 	protected $cache;
 
 	/** @var \phpbb\config\config */
 	protected $config;
 
-	/** @var \phpbb\user */
-	protected $user;
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
 
 	/** @var \phpbb\language\language */
 	protected $language;
 
+	/** @var \phpbb\notification\manager */
+	protected $notification;
+
 	/** @var \phpbb\template\template */
 	protected $template;
 
-	/** @var \phpbb\notification\manager */
-	protected $notification;
+	/** @var \phpbb\user */
+	protected $user;
 
 	/**
 	* Constructor
 	*
-	* @param \phpbb\db\driver\driver_interface $db
-	* @param \phpbb\cache\driver\driver_interface $cache
-	* @param \phpbb\config\config $config
-	* @param \phpbb\user $user
-	* @param \phpbb\language\language $language
-	* @param \phpbb\template\template $template
-	* @param \phpbb\notification\manager $notification
+	* @param \phpbb\cache\driver\driver_interface	$cache			Cache object
+	* @param \phpbb\config\config					$config			Config object
+	* @param \phpbb\db\driver\driver_interface		$db				Database object
+	* @param \phpbb\language\language				$language		Language object
+	* @param \phpbb\notification\manager			$notification	Notification manager
+	* @param \phpbb\template\template				$template		Template object
+	* @param \phpbb\user							$user			User object
 	*/
 	public function __construct(
-		\phpbb\db\driver\driver_interface $db,
 		\phpbb\cache\driver\driver_interface $cache,
 		\phpbb\config\config $config,
-		\phpbb\user $user,
+		\phpbb\db\driver\driver_interface $db,
 		\phpbb\language\language $language,
+		\phpbb\notification\manager $notification,
 		\phpbb\template\template $template,
-		\phpbb\notification\manager $notification
+		\phpbb\user $user
 	)
 	{
-		$this->db = $db;
 		$this->cache = $cache;
 		$this->config = $config;
-		$this->user = $user;
+		$this->db = $db;
 		$this->language = $language;
-		$this->template = $template;
 		$this->notification = $notification;
+		$this->template = $template;
+		$this->user = $user;
 	}
 
 	/**
-	* Event list
+	* List of phpBB's PHP events to be used
 	*
 	* @return array
 	*/
@@ -79,7 +82,7 @@ class listener implements EventSubscriberInterface
 	/**
 	* core.user_setup
 	*
-	* @param $event
+	* @param $event $event Data from the PHP event
 	*/
 	public function user_setup($event)
 	{
@@ -95,12 +98,35 @@ class listener implements EventSubscriberInterface
 	/**
 	* core.index_modify_page_title
 	*
-	* @param $event
+	* @param $event $event Data from the PHP event
 	*/
 	public function index_modify_page_title($event)
 	{
-		$anniversary_data = $this->cache->get('_vinabb_happyanniversary_data');
+		// Get the list of anniversary users
+		$anniversary_users = '';
 
+		foreach ($this->get_cache() as $user_id => $user_data)
+		{
+			$anniversary_users .= (($anniversary_users == '') ? '' : ', ') . get_username_string('full', $user_id, $user_data['username'], $user_data['user_colour']) . ' (' . $user_data['years'] . ')';
+		}
+
+		// Output
+		$this->template->assign_var('HAPPY_ANNIVERSARY_TEXT', ($anniversary_users != '') ? $this->language->lang('HAPPY_ANNIVERSARY_TEXT', $anniversary_users) : '');
+	}
+
+	/**
+	* Build cache for anniversary users, then push notifications to them once
+	*
+	* @return array
+	*/
+	protected function get_cache()
+	{
+		if (($anniversary_data = $this->driver->get('_vinabb_happyanniversary')) === false)
+		{
+			$anniversary_data = [];
+		}
+
+		// Current time
 		$time = time();
 		$now_year = date('Y', $time);
 
@@ -108,12 +134,13 @@ class listener implements EventSubscriberInterface
 		$next_time = strtotime('+1 day');
 		$expired_time = strtotime(date('d', $next_time) . '-' . date('m', $next_time) . '-'. date('Y', $next_time) . ' 00:00:00');
 
-		if ($now_year > $this->config['vinabb_happyanniversary_begin_year'] && ($anniversary_data === false || $time > $expired_time))
+		if ($now_year > $this->config['vinabb_happyanniversary_begin_year'] && (!sizeof($anniversary_data) || $time > $expired_time))
 		{
+			$anniversary_data = [];
+
 			$sql = $this->db->sql_build_query('SELECT', $this->build_sql($time));
 			$result = $this->db->sql_query($sql);
 
-			$anniversary_data = [];
 			while ($row = $this->db->sql_fetchrow($result))
 			{
 				$anniversary_data[$row['user_id']] = [
@@ -131,51 +158,22 @@ class listener implements EventSubscriberInterface
 			}
 
 			// The next day, how many seconds left?
-			$this->cache->put('_vinabb_happyanniversary_data', $anniversary_data, $expired_time - $time);
+			$this->cache->put('_vinabb_happyanniversary', $anniversary_data, $expired_time - $time);
 		}
 
-		// Get the list of anniversary users
-		$anniversary_users = '';
-		foreach ($anniversary_data as $user_id => $user_data)
-		{
-			$anniversary_users .= (($anniversary_users == '') ? '' : ', ') . get_username_string('full', $user_id, $user_data['username'], $user_data['user_colour']) . ' (' . $user_data['years'] . ')';
-		}
-
-		// Output
-		$this->template->assign_var('HAPPY_ANNIVERSARY_TEXT', ($anniversary_users != '') ? $this->language->lang('HAPPY_ANNIVERSARY_TEXT', $anniversary_users) : '');
+		return $anniversary_data;
 	}
 
 	/**
-	 * Build SQL query for anniversary users
-	 *
-	 * @param int $time Timestamp
-	 * @return array
-	 */
+	* Build SQL query for anniversary users
+	*
+	* @param int $time Timestamp
+	* @return array
+	*/
 	protected function build_sql($time)
 	{
-		// Current date
-		$now_year = date('Y', $time);
-		$now_month = date('m', $time);
-		$now_day = date('d', $time);
-
-		// Does Time Machine already exist?
-		$sql_or = '';
-		$i = $now_year - 1;
-
-		for ($i; $i >= $this->config['vinabb_happyanniversary_begin_year']; $i--)
-		{
-			$sql_or .= ($sql_or == '') ? '' : ' OR ';
-			$sql_or .= '(u.user_regdate > ' . (strtotime("{$now_day}-{$now_month}-{$i} 00:00:00") - 1) . ' AND u.user_regdate < ' . (strtotime("{$now_day}-{$now_month}-{$i} 23:59:59") + 1) . ')';
-		}
-
-		// Display anniversary of 29th february on 28th february in non-leap-years
-		if ($now_day == '28' && $now_day == '02' && !date('L', $time) && date('L', strtotime("01-01-{$i}")))
-		{
-			$sql_or .= ($sql_or == '') ? '' : ' OR ';
-			$sql_or .= '(u.user_regdate > ' . (strtotime("29-02-{$i} 00:00:00") - 1) . ' AND u.user_regdate < ' . (strtotime("29-02-{$i} 23:59:59") + 1) . ')';
-		}
-
-		$sql_and = ($sql_or == '') ? '0' : "($sql_or)";
+		$sql_or = $this->build_sql_or($time);
+		$sql_and = ($sql_or == '') ? '' : "AND ({$sql_or})";
 
 		// Build the query
 		$sql_ary = [
@@ -188,11 +186,45 @@ class listener implements EventSubscriberInterface
 				]
 			],
 			'WHERE' => '(b.ban_id IS NULL OR b.ban_exclude = 1)
-					AND ' . $this->db->sql_in_set('u.user_type', [USER_NORMAL, USER_FOUNDER]) . "
-					AND $sql_and",
-			'ORDER_BY'	=> 'u.user_regdate',
+				AND ' . $this->db->sql_in_set('u.user_type', [USER_NORMAL, USER_FOUNDER]) .
+				$sql_and,
+			'ORDER_BY'	=> 'u.user_regdate'
 		];
 
 		return $sql_ary;
+	}
+
+	/**
+	* Build SQL OR sub-query for each year
+	*
+	* @param int $time Timestamp
+	* @return string
+	*/
+	protected function build_sql_or($time)
+	{
+		$sql_or = '';
+
+		// Current date
+		$now_year = date('Y', $time);
+		$now_month = date('m', $time);
+		$now_day = date('d', $time);
+
+		// Does Time Machine already exist?
+		$i = $now_year - 1;
+
+		for ($i; $i >= $this->config['vinabb_happyanniversary_begin_year']; $i--)
+		{
+			$sql_or .= ($sql_or == '') ? '' : ' OR ';
+			$sql_or .= '(u.user_regdate > ' . (strtotime("{$now_day}-{$now_month}-{$i} 00:00:00") - 1) . ' AND u.user_regdate < ' . (strtotime("{$now_day}-{$now_month}-{$i} 23:59:59") + 1) . ')';
+		}
+
+		// Display anniversary of 29th february on 28th february in non-leap-years
+		if ($now_day == '28' && $now_month == '02' && !date('L', $time) && date('L', strtotime("01-01-{$i}")))
+		{
+			$sql_or .= ($sql_or == '') ? '' : ' OR ';
+			$sql_or .= '(u.user_regdate > ' . (strtotime("29-02-{$i} 00:00:00") - 1) . ' AND u.user_regdate < ' . (strtotime("29-02-{$i} 23:59:59") + 1) . ')';
+		}
+
+		return $sql_or;
 	}
 }
