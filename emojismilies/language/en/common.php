6bd91450238a6208a803b70ab29b458195045626
Khoa Nguyen <nedkavn@gmail.com>
2016-11-29 05:27:51 +0700

    Emoji Smilies: Ready for language strings

---
+++ emojismilies/language/en/common.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* This file is part of the Emoji Smilies package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+$lang = array_merge($lang, [
+	'EMOTICON_TEXT'	=> [
+		
+	]
+]);
