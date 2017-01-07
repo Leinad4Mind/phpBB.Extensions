46aa375f97799056c0caeffb9f6f767165c7967f
Khoa Nguyen <nedkavn@gmail.com>
2017-01-07 16:02:38 +0700

    Emoji Picker: Remove vendor files

--- emojipicker/vendor/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/emojione/emojione/src/RulesetInterface.php');
-require_once(__DIR__ . '/emojione/emojione/src/ClientInterface.php');
-require_once(__DIR__ . '/emojione/emojione/src/Client.php');
-require_once(__DIR__ . '/emojione/emojione/src/Ruleset.php');
-require_once(__DIR__ . '/emojione/emojione/src/Emojione.php');
