ccd19ea8f33f5389a6b9696f3cce75bcfaba4d79
Khoa Nguyen <nedkavn@gmail.com>
2017-01-07 15:16:20 +0700

    Add Emoji Picker 0.1.0

---
+++ emojipicker/event/listener.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* This file is part of the Emoji Picker package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\emojipicker\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Emojione\Client;
+use Emojione\Ruleset;
+
+/**
+* PHP events
+*/
+class listener implements EventSubscriberInterface
+{
+	/**
+	* List of phpBB's PHP events to be used
+	*
+	* @return array
+	*/
+	static public function getSubscribedEvents()
+	{
+		return [
+			'core.user_setup'						=> 'user_setup',
+			'core.modify_format_display_text_after'	=> 'convert_emoji_shortnames',
+			'core.modify_text_for_display_after'	=> 'convert_emoji_shortnames'
+		];
+	}
+
+	/**
+	* core.user_setup
+	*
+	* @param array $event Data from the PHP event
+	*/
+	public function user_setup($event)
+	{
+		// Add our common language variables
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = [
+			'ext_name' => 'vinabb/emojipicker',
+			'lang_set' => 'common'
+		];
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	/**
+	* Convert Emoji shortnames to images
+	*
+	* @param array $event Data from the PHP event
+	*/
+	public function convert_emoji_shortnames($event)
+	{
+		$client = new Client(new Ruleset());
+		$client->imageType = 'svg';
+		$client->sprites = false;
+
+		$event['text'] = $client->shortnameToImage($event['text']);
+	}
+}
---
+++ emojipicker/vendor/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once(__DIR__ . '/emojione/emojione/src/RulesetInterface.php');
+require_once(__DIR__ . '/emojione/emojione/src/ClientInterface.php');
+require_once(__DIR__ . '/emojione/emojione/src/Client.php');
+require_once(__DIR__ . '/emojione/emojione/src/Ruleset.php');
+require_once(__DIR__ . '/emojione/emojione/src/Emojione.php');
