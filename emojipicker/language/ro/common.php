cf07804ac1990a96e3f48756e5c87e2158ed283a
Khoa Nguyen <nedkavn@gmail.com>
2017-01-20 23:46:14 +0700

    Emoji Picker: Add Romanian translation by Oprea

---
+++ emojipicker/language/ro/common.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* This file is part of the Emoji Picker package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* Romanian Translation
+*
+* @author Oprea
+* @link http://resursele.com
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+$lang = array_merge($lang, array(
+	'EMOJI_BUTTON_EXPLAIN'	=> 'Utilizați tasta Tab pentru a insera Emoji mai repede.',
+	'EMOJI_CATS'			=> array(
+		'ACTIVITIES'	=> 'Activități',
+		'FLAGS'			=> 'Steaguri',
+		'FOOD'			=> 'Mâncare și băutură',
+		'NATURE'		=> 'Animale și natură',
+		'OBJECTS'		=> 'Obiecte',
+		'RECENT'		=> 'Recente',
+		'SMILIES'		=> 'Zâmbete și oameni',
+		'SYMBOLS'		=> 'Simboluri',
+		'TONES'			=> 'Diversitate',
+		'TRAVEL'		=> 'Călătorie și locuri'
+	)
+));
